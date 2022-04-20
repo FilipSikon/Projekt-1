6b003a7ca82ed6ece9eaf5b279391236b939fda8
Filip Sikon <filip.sikon@elev.ga.ntig.se>
2022-04-20 14:51:11 +0200

    first commit

---
+++ Eprov/Eprov/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Eprov
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("type in number:");
+
+   string answer = Console.ReadLine();
+
+   if ( {answer} < 10 == true)
+   {
+    Console.WriteLine("Value is less than 10");
+   }
+   else
+   {
+    Console.WriteLine("Value is greater than 10");
+   }
+    Console.ReadLine();
+            
+        }
+    }
+}
---
+++ Topdown/Topdown/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Raylib_cs;
+
+Raylib.InitWindow(800, 600, "2D-topdow game");
+
+Raylib.SetTargetFPS(60);
+
+while(!Raylib.WindowShouldClose())
+{
+Raylib.BeginDrawing();
+
+Raylib.EndDrawing();
+
+Raylib.DrawRectangle(100 , 100, 320, 320, Color.DARKGREEN);
+
+Raylib.ClearBackground(Color.DARKBROWN);
+
+Raylib.DrawRectangle(700 , 20, 400, 20, Color.DARKGREEN);
+
+Raylib.ClearBackground(Color.DARKBROWN);
+
+Raylib.DrawRectangle(400 , 10, 100, 500, Color.DARKGREEN);
+
+Raylib.ClearBackground(Color.DARKBROWN);
+
+Raylib.EndDrawing();
+
+}
